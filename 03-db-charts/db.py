7e4cc740495232afce8d2e8a63799e0edfa81a49
Amir Ali Khan <amir.aakhan@gmail.com>
2025-06-01 20:21:52 -0400

    db specs

--- 03-db-charts/db.py
+++ 03-db-charts/db.py
@@ -3,6 +3,6 @@
 import mysql.connector
 
 def get_connection():
-    conn = mysql.connector.connect(host="76.69.118.179", user="root", password="some_pass", db="test", port=3306)
+    conn = mysql.connector.connect(host="host", user="user", password="some_pass", db="test", port=3306)
     cursor = conn.cursor(buffered=True, dictionary=True)
     return cursor, conn
