3f14690d1cc6e43f6514e54f508981c2e3c1c080
Amir Ali Khan <amir.aakhan@gmail.com>
2025-06-01 20:11:54 -0400

    04-data

--- 04-summary-grid-filter-plot/app.py
+++ 04-summary-grid-filter-plot/app.py
@@ -8,16 +8,26 @@ uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
+    columns = df.columns.tolist()
 
-    st.subheader("Data Preview")
+
+    st.subheader("All Data")
+    st.write(df)
+
+
+    st.subheader("All Columns")
+    st.write(columns)
+
+
+    st.subheader("Data Head")
     st.write(df.head())
+
 
     st.subheader("Data Summary")
     st.write(df.describe())
 
+
     st.subheader("Filter Data")
-    columns = df.columns.tolist()
-    print(columns)
     selected_column = st.selectbox("Select column to filter by", columns)
     unique_values = df[selected_column].unique()
     selected_value = st.selectbox("Select value", unique_values)
@@ -25,11 +35,14 @@ if uploaded_file is not None:
     filtered_df = df[df[selected_column] == selected_value]
     st.write(filtered_df)
 
+
     st.subheader("Plot Data")
     x_column = st.selectbox("Select x-axis column", columns)
     y_column = st.selectbox("Select y-axis column", columns)
 
     if st.button("Generate Plot"):
         st.line_chart(filtered_df.set_index(x_column)[y_column])
+
+        
 else:
     st.write("Please upload data file")
